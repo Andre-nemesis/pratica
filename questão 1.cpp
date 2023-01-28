fd34478cfefe82280306c938da91114f52b29b1f
Andre-nemesis <105876843+Andre-nemesis@users.noreply.github.com>
2023-01-28 20:03:53 -0300

    Update questão 1.cpp

--- questão 1.cpp
+++ questão 1.cpp
@@ -1,32 +1,34 @@
 #include <stdio.h>
+#include <stdio.h>
 #define Vaa 10
 
 int main () {
+	setlocale(LC_ALL,"Portuguese_Brazil");
 	int Va[Vaa],maior,menor, maiorIn,menorIn,i;
 	
 	printf("Digite 10 valores abaixo.\n\n");
 	
 	
 	for(i=0;i<Vaa;i++){
-		printf("Digite o %d numero.\n",i + 1);
-		scanf("%f",&Va[i]);
+		printf("Digite o %d número.\n",i + 1);
+		scanf("%d",&Va[i]);
 	}
 	
 	maior = Va[0];
 	menor = Va[0];
 	
 	for(i = 0;i<Vaa;i++) {
-		if(Va[i]>maior) {
+		if(Va[i]>=maior) {
 			maior = Va[i];
 			maiorIn = i;
 		}
 		
-		if(Va[i]<menor) {
+		if(Va[i]<=menor) {
 			menor = Va[i];
 			menorIn = i;
 		}
 	}
 	
-	printf("O menor numero: %d seu indice %d.\nO maior numero: %d seu indice.\n",menor,menorIn,maior,maiorIn);
+	printf("O menor número: %d seu indice %d.\nO maior número: %d seu indice %d.\n",menor,menorIn,maior,maiorIn);
 	return 0;
 }
